5f3f946d945d74e561364847f0ca8098c7e52df5
Rgustave <mugustave02@gmail.com>
2017-04-30 23:29:32 -0400

    configuration of mongolab

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
 var express = require('express');
-var mongoose = require('mongoose');
+var mongodb = require("mongodb");
 var bodyParser =  require('body-parser');
+var ObjectID = mongodb.ObjectID;
+var CUSTOMER_COLLECTION = "customers";
 
-mongoose.connect('mongodb://localhost/rest-api');
+
 
 var app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
+var db;
+
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+db = database;
+
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
 
 app.use('/api',require('./routes/api'))
 
-app.listen(3000);
+// app.listen(3000);
 console.log('API is running on port 3000');
